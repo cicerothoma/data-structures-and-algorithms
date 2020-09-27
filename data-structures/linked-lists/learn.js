89c1e4f895566b7b21970904a4032ca2f500e712
Collins Thomas <thomascollins582@gmail.com>
2020-09-27 23:42:50 +0100

    added prepend method to linked list

--- data-structures/linked-lists/learn.js
+++ data-structures/linked-lists/learn.js
@@ -12,6 +12,13 @@
 // Create a method that adds value to the end of the linked list created below
 // Create a method that adds value at the beginning of the linked list created below
 
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
 class LinkedList {
   constructor(value) {
     this.head = {
@@ -39,10 +46,22 @@ class LinkedList {
   }
 
   // Solution (Prepend)
+  prepend(value) {
+    const newNode = new Node(value);
+    // Changes the next value in our new node to be a pointer to the head node
+    newNode.next = this.head;
+    // Change the head Node to the new node
+    this.head = newNode;
+    this.length += 1;
+
+    return this;
+  }
 }
 
 const myLinkedList = new LinkedList(10);
 console.log(myLinkedList.append(5));
 console.log(myLinkedList.append(16));
+console.log(myLinkedList.prepend(1));
+console.log(myLinkedList.prepend(20));
 
 // console.log(myLinkedList);
